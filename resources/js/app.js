b6bf46117a91e2790cdf93ab1059937726069016
magic_in_code <magicincode@gmail.com>
2025-02-03 13:48:42 +0200

    Laravel

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+import { createApp } from 'vue';
+import FormComponent from './components/FormComponent.vue';
+
+const app = createApp({});
+app.component('form-component', FormComponent);
+app.mount('#app');
