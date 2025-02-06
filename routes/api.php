40eb7123c54501cbab8807dcb91f42b44f99474d
magic_in_code <magicincode@gmail.com>
2025-02-06 20:24:23 +0200

    Feature(routes)

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ZohoController;
+
+Route::group(['prefix' => 'api'], function () {
+    Route::post('/create-account', [ZohoController::class, 'createAccount']);
+    Route::post('/create-deal', [ZohoController::class, 'createDeal']);
+});
+
+
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/zoho-crm', function () {
+    return view('zoho-crm');
+});
+
+Route::get('/account-form', function () {
+    return view('account.create-form');
+});
+Route::get('/deal-form', function () {
+    return view('deal.create-form');
+});
