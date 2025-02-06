5d2d28f99c43390399c06dabfb5812d0c4f4f7e2
magic_in_code <magicincode@gmail.com>
2025-02-06 20:26:04 +0200

    Feature(outer) configs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,20 @@
 import './bootstrap';
 import { createApp } from 'vue';
-import FormComponent from './components/FormComponent.vue';
+import AccountComponent from './components/account/CreateAccount.vue';
+import DealComponent from './components/deal/CreateDeal.vue';
 
-const app = createApp({});
-app.component('form-component', FormComponent);
-app.mount('#app');
+// Если на странице есть контейнер для формы аккаунта
+if (document.getElementById('app-account-create')) {
+    const accountApp = createApp({});
+    accountApp.component('create-account', AccountComponent);
+    accountApp.mount('#app-account-create');
+}
+
+
+
+// Если на странице есть контейнер для формы сделки
+if (document.getElementById('app-deal-create')) {
+    const dealApp = createApp({});
+    dealApp.component('create-deal', DealComponent);
+    dealApp.mount('#app-deal-create');
+}
