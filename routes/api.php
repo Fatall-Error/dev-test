03d210ab4bc21775967b219adda7c2b8b5ecaae8
magic_in_code <magicincode@gmail.com>
2025-02-09 00:32:48 +0200

    Feature(routes) api routes

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ZohoController;
 
-Route::group(['prefix' => 'api'], function () {
-    Route::post('/create-account', [ZohoController::class, 'createAccount']);
-    Route::post('/create-deal', [ZohoController::class, 'createDeal']);
-});
+
+Route::post('/create-account', [ZohoController::class, 'createAccount']);
+Route::post('/create-deal', [ZohoController::class, 'createDeal']);
+
 
 
