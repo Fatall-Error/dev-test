4f0c29786bf3129be516d2a03c30928587d9b2c6
magic_in_code <magicincode@gmail.com>
2025-02-10 16:59:21 +0200

    Style (ZOHO) zoho crm menu style

--- resources/views/zoho-crm.blade.php
+++ resources/views/zoho-crm.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRM Форма</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     @else
@@ -14,9 +17,27 @@
 </head>
 <body>
 <div id="app">
-    <a href="/account-form">Перейти до Створення аккаунту</a>
-    <a href="/deal-form">Перейти до Створення Угоди</a>
+    <section>
+        <div class="container mt-4">
+            <div class="list-group">
+                <a href="/account-form" class="list-group-item list-group-item-action">
+                    <i class="bi bi-person-plus me-2"></i> Перейти до Створення аккаунту
+                </a>
+                <a href="/deal-form" class="list-group-item list-group-item-action">
+                    <i class="bi bi-briefcase me-2"></i> Перейти до Створення Угоди
+                </a>
+            </div>
+        </div>
+    </section>
 </div>
+
+<style>
+    .list-group {
+        max-width: 550px;
+        margin:  100px auto;
+        border-radius: 10px;
+    }
+</style>
 
 </body>
 </html>
